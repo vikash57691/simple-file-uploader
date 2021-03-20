1e129ff39f626a01fc66b64b2c4524f083d5e2a8
vikash57691 <64069509+vikash57691@users.noreply.github.com>
2021-03-20 23:41:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
   port: (process.env.PORT || 5000),
   s3: {
-    key: '',
-    secret: '',
-    bucket: ''
+    key: 'AKIAWHGPWVHUTYM3P3VV',
+    secret: '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',
+    bucket: 'demostorageanonpe'
   },
-  s3_enabled: false,
+  s3_enabled: true,
   upload_dir: './uploads'
 };
